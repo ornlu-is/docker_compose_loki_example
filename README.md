fc02ed4276f5bd3cbad22e73c4ffc88a8d0a5762
ornlu-is <luismbfranco95@gmail.com>
2023-08-11 15:38:55 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# docker_compose_loki_example
+# Docker Compose Loki example
+
+A simple example on how to set up Loki, Promtail, and Grafana for a Docker Compose environment.
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	for {
+		log.Println("Oh! Now we see the violence inherent in the system! Help, help, I'm being repressed!")
+		time.Sleep(1 * time.Second)
+	}
+}
